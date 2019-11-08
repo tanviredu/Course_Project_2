8386b6d7cacb3e52e4130dc70d4912454b22f837
mechanic <mechanic@localhost.localdomain>
2019-11-08 12:00:26 -0500

    added

--- startbootstrap-clean-blog-gh-pages/asset/config.php
+++ startbootstrap-clean-blog-gh-pages/asset/config.php
@@ -2,10 +2,10 @@
 	//start thr ob 
 	ob_start();
 	// define the global variable
-	defined("DB_HOST") ? null : define("DB_HOST",'localhost');
-	defined("DB_USER") ? null : define("DB_USER",'root');
-	defined("DB_PASS") ? null : define("DB_PASS",'');
-	defined("DB_NAME") ? null : define("DB_NAME",'blog');
+	defined("DB_HOST") ? null : define("DB_HOST",'sql111.epizy.com'); //for loaal "localhost"
+	defined("DB_USER") ? null : define("DB_USER",'epiz_24766495');	// for local root
+	defined("DB_PASS") ? null : define("DB_PASS",'passwordTanvir123');// no pass for local
+	defined("DB_NAME") ? null : define("DB_NAME",'epiz_24766495_blog');	// for local "blog"
 	//make a connection
 	$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 	if(!$connection){
